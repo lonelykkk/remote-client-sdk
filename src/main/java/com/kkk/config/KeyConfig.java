bc24209f500a29acd2ae85daf6f2efffed5d1fc3
lonelykkk <2765314967@qq.com>
2024-12-09 09:04:59 +0800

    KeyConfig类减去部分依赖

--- src/main/java/com/kkk/config/KeyConfig.java
+++ src/main/java/com/kkk/config/KeyConfig.java
@@ -1,7 +1,6 @@
 package com.kkk.config;
 
 import com.kkk.client.RemoteClient;
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ConfigurationProperties("kkk.client")
-@Data
 @ComponentScan
 public class KeyConfig {
     private String apiCode;
@@ -25,5 +23,29 @@ public class KeyConfig {
     @Bean
     public RemoteClient remoteClient() {
         return new RemoteClient(apiCode, qq, qqMailCode);
+    }
+
+    public String getApiCode() {
+        return apiCode;
+    }
+
+    public void setApiCode(String apiCode) {
+        this.apiCode = apiCode;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getQqMailCode() {
+        return qqMailCode;
+    }
+
+    public void setQqMailCode(String qqMailCode) {
+        this.qqMailCode = qqMailCode;
     }
 }
